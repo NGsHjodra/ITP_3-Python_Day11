796311e81f7a72d9b9506e3c607ed6e813e320b1
Niyakit <niyakit123@gmail.com>
2023-03-27 12:24:46 +0700

    update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 sudo pkill gunicorn
-docker compose down
-docker compose up -d --build
+sudo docker compose down
+sudo docker compose up -d --build
 pip install -r requirements.txt
 echo "Sleeping for 10 seconds to allow the database to start up..."
 sleep 10
