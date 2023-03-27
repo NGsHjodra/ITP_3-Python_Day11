436c649e968daf139880d63cfa21de23485b143f
Niyakit <niyakit123@gmail.com>
2023-03-27 12:31:37 +0700

    update test-db.py

--- test-db.py
+++ test-db.py
@@ -29,8 +29,8 @@ def main():
     # insert a new products
     logging.info('Inserting data...')
     for product in products:
-        if product[1] < 40000.00:
-            cur.execute(f"INSERT INTO products (name, price) VALUES ('{product[0]}', '{product[1]}')")
+        # if product[1] < 40000.00:
+        cur.execute(f"INSERT INTO products (name, price) VALUES ('{product[0]}', '{product[1]}')")
 
     conn.commit()
 
