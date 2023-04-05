c8b1030e2402a33ec1a28426f49f271999636866
Niyakit <niyakit123@gmail.com>
2023-04-05 16:13:45 +0700

    update update.sh 1613

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ sudo docker compose up -d --build
 pip install -r requirements.txt
 echo "Sleeping for 10 seconds to allow the database to start up..."
 sleep 10
+sudo docker ps
 python3 test-db.py
 /home/ubuntu/.local/bin/gunicorn -b 0.0.0.0:8888 mini-server:app --daemon
 # sleep 5
